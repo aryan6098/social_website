d59f503a205c96ec9b50570417a567c768d902ca
Aryan Raj <araj60988@gmail.com>
2021-05-13 22:07:27 +0530

    Updates

---
+++ precache-manifest.e120b4da6ff15190be2f6c7e796c64a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cbc061a0424276c150e8d5236e49ca",
+    "url": "/social_website/index.html"
+  },
+  {
+    "revision": "ca85c45b17c7266dd72e",
+    "url": "/social_website/static/css/main.4990e117.chunk.css"
+  },
+  {
+    "revision": "cf77fbccdf32e0ad3f09",
+    "url": "/social_website/static/js/2.6fd9b1de.chunk.js"
+  },
+  {
+    "revision": "fdca082fd0c4ef1db4a5b3f48c425822",
+    "url": "/social_website/static/js/2.6fd9b1de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca85c45b17c7266dd72e",
+    "url": "/social_website/static/js/main.dfcef601.chunk.js"
+  },
+  {
+    "revision": "f87c275a07cd5effb7c1",
+    "url": "/social_website/static/js/runtime-main.a304f7cd.js"
+  }
+]);
